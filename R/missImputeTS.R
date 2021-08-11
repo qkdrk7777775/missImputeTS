28941dc7cfca6c16afa04561087a724ef2906e3a
cjcho <qkdrk777777@naver.com>
2021-08-11 19:18:32 +0900

    error remove

--- R/missImputeTS.R
+++ R/missImputeTS.R
@@ -1,14 +1,15 @@
-#' missImputeTS
+#' Daily Closing Prices of Major European Stock Indices, 1991–1998
 #'
-#' @name missImputeTS
-#' @description imputation packages
-#' @docType package
-NULL
-
-#' @docType data
-#' @keywords datasets
-#' @name EuStockMarkets2
-#' @description Contains the daily closing prices of major European stock indices: Germany DAX (Ibis), Switzerland SMI, France CAC, and UK FTSE. The data are sampled in business time, i.e., weekends and holidays are omitted.
-#' @usage data(EuStockMarkets2)
-#' @format A multivariate time series with 1860 observations on 4 variables. The object is of class "mts".
-#' @source The data were kindly provided by Erste Bank AG, Vienna, Austria.
+#' Contains the daily closing prices of major European stock indices: Germany DAX (Ibis), Switzerland SMI, France CAC, and UK FTSE. The data are sampled in business time, i.e., weekends and holidays are omitted.
+#'
+#' @format A data frame with 1860 rows and 5 variables:
+#' \describe{
+#'   \item{times}{price, in US dollars}
+#'   \item{DAX}{Germany}
+#'   \item{SMI}{Switzerland}
+#'   \item{CAC}{France}
+#'   \item{FTSE}{UK}
+#'   ...
+#' }
+#' @source \url{The data were kindly provided by Erste Bank AG, Vienna, Austria.}
+"EuStockMarkets2"
